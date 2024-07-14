52b9733bfce8cec046abeb9909acf6c8b5b39638
Mohammed-Abed Hassani-Idrissi <mhassani@c3r4p5.1337.ma>
2024-07-14 23:07:51 +0100

    more updates on home nav bar

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const link = document.querySelector("a");
 function linkBehavior(event)
 {
     event.preventDefault();
+    link.style.backgroundColor = "#3f2455";
+    link.style.borderRadius = "10%";
+    link.style.boxShadow = "0px 0px 10px #9477fa"
+    link.style.paddingTop = "5px";
+    link.style.height = "50px";
 }
 
 link.addEventListener("click", linkBehavior);
