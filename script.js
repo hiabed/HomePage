e25bdb999b608733697118cdd8bdd0051c9e6e04
Mohammed-Abed Hassani-Idrissi <mhassani@c3r3p9.1337.ma>
2024-07-15 23:42:43 +0100

    more updates in design

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const link = document.querySelectorAll("a");
 
+let j = 0;
+if (j === 0)
+{
+    link[j].classList.add('link');
+    j++;
+}
+
 for (let i = 0; i < link.length ;i++)
 {
     link[i].addEventListener("click", (event)=>
